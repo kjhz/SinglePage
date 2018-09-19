69040136c6f0ad55b39ceef365426c5956dd4b03
kjhz <350697470@qq.com>
2018-09-19 10:15:00 +0800

    解决导航条消失的bug，增加屏幕滑动效果

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
-/* 手机端导航栏的滑动效果 */
-$(document).ready(function () {
+(function () {
+    'use strict';
+    /* 手机端导航栏的滑动效果 */
     $("#menu button.navbar-toggle").click(function () {
         $("#menu div.navbar-collapse").slideToggle("fast");
     });
-});
 
-/* 滑动图片 */
-const sliderImages = document.querySelectorAll('.slide-in');
-
-function checkSlide(e) {
-    const clientHeight = window.innerHeight
-        ,bottom = clientHeight * 0.2;
-
-    sliderImages.forEach(sliderimage => {
-        // 取得元素的的位置和高度
-        let box = sliderimage.getBoundingClientRect();
-        let offsetHeight = box.top + box.height + bottom;
-        //元素出现在视窗时执行滑动效果
-        if (offsetHeight < clientHeight) {
-            sliderimage.classList.add('active');
+    $('a.page-scroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 900);
+                return false;
+            }
         }
     });
-}
-// 监听滚动
-window.addEventListener('scroll', checkSlide);
+
+    /* 滑动图片 */
+    const sliderImages = document.querySelectorAll('.slide-in');
+    function checkSlide(e) {
+        const clientHeight = window.innerHeight;
+
+        sliderImages.forEach(sliderimage => {
+            // 取得元素的的位置和高度
+            let box = sliderimage.getBoundingClientRect();
+
+            //元素出现在视窗时执行滑动效果
+            if ((box.top + box.height < clientHeight) && box.top > 0) {
+                sliderimage.classList.add('active');
+            } else {
+                sliderimage.classList.remove('active');
+            }
+        });
+    }
+    // 监听滚动
+    window.addEventListener('scroll', checkSlide);
+})();
+
